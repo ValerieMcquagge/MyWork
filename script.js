83f6f22f56538c168cfff1dc49144b6e6d53cb7a
ValerieM <79781142+ValerieMcquagge@users.noreply.github.com>
2026-07-09 18:03:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-console.log("Portfolio Loaded!");
+// Typing animation
+
+const words = [
+"Data Analyst",
+"UX Researcher",
+"Technology Enthusiast",
+"Problem Solver"
+];
+
+
+let wordIndex = 0;
+let letterIndex = 0;
+
+const typing = document.getElementById("typing");
+
+
+function type(){
+
+if(letterIndex < words[wordIndex].length){
+
+typing.textContent += words[wordIndex][letterIndex];
+
+letterIndex++;
+
+setTimeout(type,100);
+
+}
+
+else{
+
+setTimeout(deleteWord,1500);
+
+}
+
+}
+
+
+
+function deleteWord(){
+
+if(letterIndex > 0){
+
+typing.textContent =
+words[wordIndex].substring(0,letterIndex-1);
+
+letterIndex--;
+
+setTimeout(deleteWord,50);
+
+}
+
+else{
+
+wordIndex++;
+
+if(wordIndex >= words.length){
+
+wordIndex=0;
+
+}
+
+setTimeout(type,500);
+
+}
+
+}
+
+
+type();
